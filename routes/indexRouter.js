dca62577740d7dfa9b3f0442eba1568230cad7b0
Ollie Preiss <76745659+OlliePreiss@users.noreply.github.com>
2024-10-03 16:58:28 +0100

    mini messaging board app created

---
+++ routes/indexRouter.js
@@ -0,0 +1,30 @@
+const { Router } = require('express');
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello world!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+const indexRouter = Router();
+
+indexRouter.get("/",(req, res) => {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+})
+indexRouter.get("/new", (req, res) => {
+  res.render("form");
+})
+indexRouter.post("/new", (req, res) => {
+  messages.push({ text: req.body.message, user: req.body.name, added: new Date()});
+  res.redirect("/");
+})
+
+
+module.exports = indexRouter;
